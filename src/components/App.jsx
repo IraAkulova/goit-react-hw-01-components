7f7001c60546cbc93b9a2b8f355fad8e5f7de9ec
Ira Akulova <ira.akulova2019@gmail.com>
2023-03-07 16:02:29 +0200

    111

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
 import { Profile } from 'components/profile/Profile';
-import user from '../user.json'
+
 
 export const App = () => {
   return (
     <div>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
+      <Profile/>
     </div>
   );
 };
