087fd257a86e4d18f16fc6741adbbf876da16f61
Ira Akulova <ira.akulova2019@gmail.com>
2023-03-10 13:36:33 +0200

    final refactoring

--- src/components/transactionHistory/TransactionHistory.jsx
+++ src/components/transactionHistory/TransactionHistory.jsx
@@ -1,13 +1,25 @@
 import PropTypes from 'prop-types';
-import { Header } from './header/Header';
-import { Table } from './table/Table';
 import css from './TransactionHistory.module.css';
 
 export function TransactionHistory({ items }) {
   return (
     <table className={css.transactionHistory}>
-      <Header />
-      <Table transactions={items} />
+      <thead className={css.tableHeader}>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody className={css.tableBody}>
+        {items.map(transaction => (
+          <tr className={css.row} key={transaction.id}>
+            <td className={css.type}>{transaction.type}</td>
+            <td className={css.amount}>{transaction.amount}</td>
+            <td className={css.currency}>{transaction.currency}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 }
